79548dd66df90b598717f2878f3382288563b4b6
fsf4965 <fsf4965@autuni.ac.nz>
2021-03-11 12:28:59 +1300

    Commit with new branch

---
+++ test2.java
@@ -0,0 +1,15 @@
+    import java.util.Scanner;
+    public class Simple
+    {  
+        public static void main(String args[])
+        {  
+         System.out.println("Hello Java");  
+
+         Scanner reader = new Scanner(System.in);
+         System.out.print("Enter a number: ");
+
+         int number = reader.nextInt();
+
+         System.out.println("You entered: " + number);
+        }  
+    }  
